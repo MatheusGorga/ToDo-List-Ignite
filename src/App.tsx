ffadec4b3a7224fa3d0d94ff03ac3dce2b5addff
MatheusGorga <matheusgobar@gmail.com>
2022-07-15 17:55:44 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,94 @@ import { Header } from './components/Header/Header';
 import { Todo } from './components/Todo/Todo';
 import './global.css';
 import styles from './App.module.css';
-import { CreateTodo } from './components/CreateTodo/CreateTodo';
+import { FormEvent, useState } from 'react';
+import { PlusCircle } from 'phosphor-react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [newTextTodo, setNewTextTodo] = useState('');
+
+  const [tasks, setTasks] = useState([
+    {
+      id: uuidv4(),
+      todo: 'Aprovado',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      todo: 'Começar',
+      completed: true,
+    },
+  ]);
+
+  function handleCreateTodo(event: FormEvent) {
+    event.preventDefault();
+    const text = newTextTodo;
+    setTasks([...tasks, { id: uuidv4(), todo: text, completed: false }]);
+  }
+
+  function deleteTodo(idDelete: string) {
+    const DeleteAll = tasks.filter((task) => task.id != idDelete);
+    setTasks(DeleteAll);
+  }
+
+  function isCompleted(idCompleted: string) {
+    const newListTask = tasks.map((task) =>
+      task.id == idCompleted
+        ? {
+            ...task,
+            completed: !task.completed,
+          }
+        : task
+    );
+
+    setTasks(newListTask);
+  }
+
   return (
-    <>
+    <div>
       <Header />
       <div className={styles.container}>
-        <CreateTodo />
+        <div className={styles.form}>
+          <form onSubmit={handleCreateTodo}>
+            <input
+              onChange={(e) => setNewTextTodo(e.target.value)}
+              placeholder='Adicione uma nova tarefa'
+              type='text'
+            />
+            <button type='submit'>
+              Criar <PlusCircle size={24} />
+            </button>
+          </form>
+        </div>
 
         <div className={styles.info}>
           <section>
-            <p>Tarefas criadas</p> <span>0</span>
+            <p>Tarefas criadas</p> <span>{tasks.length}</span>
           </section>
 
           <section>
             <p>Concluídas</p>
-            <span>0 de 0</span>
+            <span>
+              {tasks.filter((task) => task.completed == true).length} de{' '}
+              {tasks.length}
+            </span>
           </section>
         </div>
-        <Todo />
+        {tasks.map((task) => {
+          return (
+            <Todo
+              key={task.id}
+              id={task.id}
+              todoMessage={task.todo}
+              completed={task.completed}
+              onDeleteTodo={deleteTodo}
+              onCompleted={isCompleted}
+            />
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -2,24 +2,38 @@ import { CheckCircle, Circle, Trash } from 'phosphor-react';
 import { useState } from 'react';
 import styles from './Todo.module.css';
 
-export function Todo() {
-  const [finish, setFinish] = useState(false);
+interface todoProps {
+  id: string | any;
+  todoMessage: string;
+  completed: true | false;
+  onDeleteTodo: (id: string) => void;
+  onCompleted: (id: string) => void;
+}
 
-  function handleFinish() {
-    setFinish(!finish);
+export function Todo({
+  id,
+  todoMessage,
+  completed,
+  onDeleteTodo,
+  onCompleted,
+}: todoProps) {
+  function handleDelete() {
+    onDeleteTodo(id);
+  }
+
+  function isCompleted() {
+    onCompleted(id);
   }
 
   return (
     <div className={styles.todo}>
-      <button title='finalizado' onClick={handleFinish}>
-        {finish ? <CheckCircle size={24} /> : <Circle size={24} />}
+      <button title='finalizado' onClick={isCompleted}>
+        {completed ? <CheckCircle size={24} /> : <Circle size={24} />}
       </button>
-      <span>
-        {' '}
-        sakdjalkjsdklajsdlkajsdlkjalsdkjalskdjlaksjsakdjalkjsdklajsdlkajsdlkjalsdkjalskdjlaksj{' '}
-        sakdjalkjsdklajsdlkajsdlkjalsdkjalskdjlaksj{' '}
+      <span className={completed ? `${styles.completed}` : ''}>
+        {todoMessage}
       </span>
-      <button title='Deletar comentário'>
+      <button title='Deletar comentário' onClick={handleDelete}>
         <Trash size={24} />
       </button>
     </div>
